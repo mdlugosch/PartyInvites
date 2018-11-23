20bcbefc1c464546602e36b7a888c15a07c4d4ea
Michael Dlugosch <michael.dlugosch@web.de>
2018-11-23 11:27:21 +0100

    Test1

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -8,7 +8,7 @@ namespace PartyInvites.Models
     public class GuestResponse
     {
         public long Id { get; set; }
-
+           
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
